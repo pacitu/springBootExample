0453e956c48a0b1468b9cfbf54e0313e5391e592
Pavel Kostadinov <pacitu@Pavels-MacBook-Pro.local>
2015-09-10 01:14:52 +0300

    Add DB migration and initial gradle config.
    Signed-off-by: Pavel Kostadinov <pacitu@Pavels-MacBook-Pro.local>

---
+++ src/main/resources/db/migration/V1__add_table_user.sql
@@ -0,0 +1,11 @@
+CREATE TABLE user (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  date_of_birth TIMESTAMP NOT NULL,
+  created_at TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE INDEX (email)
+) ENGINE=INNODB;
